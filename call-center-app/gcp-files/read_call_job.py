48f5e093d08dfb3008d76d63f17fa4be9a6920ec
Andy Goade <9042107+ajgoade@users.noreply.github.com>
2020-09-01 13:03:31 -0500

    Add docker config and variable to process multiple cloud providers

--- call-center-app/gcp-files/read_call_job.py
+++ call-center-app/gcp-files/read_call_job.py
@@ -4,17 +4,22 @@ import time
 from datetime import datetime
 # from pydub import AudioSegment
 from os import environ
-
+#Cassandra / Astra imports
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import Cluster
+#Google Cloud imports
 from google.cloud import speech
 from google.cloud.speech import types
 # from google.cloud import storage
+#Amazon imports
+import boto3
+import requests
 
 global mediafileurl
 global mediaformat
 global jobname
 secureconnect = ''
+cloudlocation = ''
 
 #def mp3_to_wav(audio_file_name):
     #Not currently used
@@ -102,6 +107,70 @@ def google_sentiment(jobid, transcript):
     session.shutdown()
 
 
+def amazon_transcribe(audio_file_name, jobid):
+    aws_uri = audio_file_name
+    transcript = ''
+
+    transcribe_client = boto3.client('transcribe',  region_name='us-east-1')
+    transcribe_client.start_transcription_job(TranscriptionJobName=jobid, Media={'MediaFileUri': aws_uri},
+                                       MediaFormat='wav', LanguageCode='en-US')
+
+    while True:
+        status = transcribe_client.get_transcription_job(TranscriptionJobName=jobid)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        #print("Not ready yet...")
+        time.sleep(10)
+    #print(status)
+
+    response = requests.get(status['TranscriptionJob']['Transcript']['TranscriptFileUri']).json()
+    transcript = response['results']['transcripts'][0]['transcript']
+
+    # print("Writing transcript results for "+str(jobid)+" .....")
+    cloud_config = {'secure_connect_bundle': secureconnect}
+    auth_provider = PlainTextAuthProvider('callcenter', 'datastax')
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session = cluster.connect()
+    session.execute(
+        f'update callcenter.call_center_voice_source set last_updated=%s, process_status=%s, transcript=%s where call_id={jobid}',
+        (datetime.utcnow(), 'aws_complete', transcript))
+
+    #threading.Thread(target=amazon_sentiment, args=(jobid, transcript)).start()
+
+    session.shutdown()
+
+
+def amazon_sentiment(jobid, transcript):
+    from google.cloud import language
+    from google.cloud.language import enums
+    from google.cloud.language import types
+
+    client = language.LanguageServiceClient()
+
+    document = types.Document(
+        content=transcript,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    annotations = client.analyze_sentiment(document=document)
+
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    final_sentiment = 'Overall Sentiment: score of {:+.2f} with magnitude of {:+.2f}'.format(
+        score, magnitude)
+
+    # print("Writing transcript results for "+str(jobid)+" .....")
+    cloud_config = {'secure_connect_bundle': secureconnect}
+    auth_provider = PlainTextAuthProvider('callcenter', 'datastax')
+    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    session = cluster.connect()
+    session.execute(
+        f'update callcenter.call_center_voice_source set last_updated=%s, process_status=%s, sentiment=%s where call_id={jobid}',
+        (datetime.utcnow(), 'complete', final_sentiment))
+
+    session.shutdown()
+
+
 def get_transactions():
     # Connect to Astra and Run query
     #print("Connecting to DataStax Astra .....")
@@ -121,25 +190,25 @@ def get_transactions():
     for row in rows:
         mediafileurl = row.call_link
         jobid = row.call_id
-        cloudlocation = mediafileurl.split(':')[0]
 
-        if cloudlocation == 'gs':
+        if cloudlocation == 'gcp':
             threading.Thread(target=google_transcribe,args=(mediafileurl, jobid)).start()
             session.execute(
                 f'update callcenter.call_center_voice_source set last_updated=%s, process_status=%s where call_id={jobid}',
                 (datetime.utcnow(), 'transcribe_scheduled'))
 
-        elif cloudlocation == 's3':
-            print("s3 not available yet")
+        elif cloudlocation == 'aws':
+            threading.Thread(target=amazon_transcribe, args=(mediafileurl, jobid)).start()
+            #print("s3 not available yet")
             session.execute(
                 f'update callcenter.call_center_voice_source set last_updated=%s, process_status=%s where call_id={jobid}',
-                (datetime.utcnow(), 's3_unavailable'))
+                (datetime.utcnow(), 'transcribe_scheduled'))
 
-        elif cloudlocation == 'wasbs':
+        elif cloudlocation == 'azure':
             print("wasbs not available yet")
             session.execute(
                 f'update callcenter.call_center_voice_source set last_updated=%s, process_status=%s where call_id={jobid}',
-                (datetime.utcnow(), 'wasbs_unavailable'))
+                (datetime.utcnow(), 'azure_unavailable'))
 
         #print("Job scheduled "+str(jobid)+" .....")
 
@@ -149,6 +218,7 @@ def get_transactions():
 def main():
     import argparse
     global secureconnect
+    global cloudlocation
 
     #pdb.set_trace()
     parser = argparse.ArgumentParser()
@@ -158,9 +228,13 @@ def main():
                         help="Location of Cloud providor's connection package")
     parser.add_argument("--interval", type=int, default=60,
                         help="Interval to pause before checking for new transactions")
+    parser.add_argument("--cloud", type=str, default='gcp',
+                        help="Cloud provider app is running in: gcp (default), aws, azure")
     args = parser.parse_args()
     secureconnect = args.secure_connect
     waittime = args.interval
+    cloudlocation = args.cloud
+
     if args.creds is not None:
         environ["GOOGLE_APPLICATION_CREDENTIALS"] = args.creds
 
